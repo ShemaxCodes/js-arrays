c8bf5526fd79f611752b085a894fb6cb41977bfa
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2021-11-26 11:41:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+// Another Way
+
+const years = new Array(1991, 1988, 2008, 2020);
+console.log(years);
+
+console.log(friends[0]); // Grabs the name Michael
